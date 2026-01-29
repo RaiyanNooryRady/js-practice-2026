d2374dfe6cbedb18c39d03b0e900af30bf8f851b
raiyannooryrady <raiyannooryrady@gmail.com>
2026-01-29 12:33:54 +0600

    Add function examples demonstrating return types and function as values in function.js

---
+++ scripts/function.js
@@ -0,0 +1,26 @@
+//function  without return type
+function sum(a,b){
+    console.log('sum:',a+b);
+}
+sum(5,10);
+
+//function with return type
+
+function sumR(a,b){
+    return a+b;
+}
+let result=sumR(5,15);
+console.log('sum with return:',result);
+
+//Any statement after return will not execute
+function hello(param1,param2){
+    console.log('before return');
+    return "Hello " +param1+ " "+param2;
+    console.log('after return'); //this will not execute
+}
+let greetings=hello('world','!');
+console.log(greetings);
+
+let fun=hello; //function as value
+console.log(fun, typeof fun);
+
--- scripts/operator.js
+++ scripts/operator.js
@@ -3,4 +3,4 @@ let y= "10" - 2;
 let z=x+y;
 console.log(x,y,z); //410
 console.log(typeof x, typeof y, typeof z); //number number number
-// test
+// testn
