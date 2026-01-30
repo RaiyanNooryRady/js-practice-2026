423285fde82b309e5ddf708b2ef046078726aca7
raiyannooryrady <raiyannooryrady@gmail.com>
2026-01-30 11:28:51 +0600

    Add string.js to demonstrate string creation and type comparison

---
+++ scripts/string.js
@@ -0,0 +1,12 @@
+//let text= "A brown fox jumps over the \"Lazy Dog\"";
+//let text= `A brown fox jumps over the "Lazy Dog"`;
+let text = "Hello world \Vesting\tjhjhj check";
+console.log(text);
+let x='abcd';
+let y=new String('abcd');
+console.log('x value:',x);
+console.log('x type:',typeof x);
+console.log('y value:',y);
+console.log('y type:',typeof y);
+console.log(x==y); //true
+console.log(x===y); //false
