075454f3ee073a8e787c2f0029125b89ca1449cd
raiyannooryrady <raiyannooryrady@gmail.com>
2026-01-30 23:03:27 +0600

    Update string.js to modify text examples and enhance string manipulation demonstrations

--- scripts/string.js
+++ scripts/string.js
@@ -1,12 +1,25 @@
 //let text= "A brown fox jumps over the \"Lazy Dog\"";
-//let text= `A brown fox jumps over the "Lazy Dog"`;
-let text = "Hello world \Vesting\tjhjhj check";
+let text= `A Special brown fox jumps over the "Lazy Dog"`;
+//let text = "Hello world \Vesting\testing check";
 console.log(text);
 let x='abcd';
 let y=new String('abcd');
+let z= new String('abcd');
 console.log('x value:',x);
 console.log('x type:',typeof x);
 console.log('y value:',y);
 console.log('y type:',typeof y);
 console.log(x==y); //true
 console.log(x===y); //false
+console.log(z===y);
+console.log(y==z); //false
+
+console.log(text.slice(2,7));
+console.log(text.slice(-9,-1)); //negative index
+console.log(text.slice(4)); //till end
+
+console.log(text.substring(2,7)); //negative index not allowed
+console.log(text.substr(2,7)); //second parameter is length
+console.log(text.substr(4)); //till end
+
+console.log(text.replace("brown", "black")); //case sensitive
