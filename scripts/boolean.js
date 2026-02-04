119eb1732b680afef86fbb9ad8f46d56f7f29138
raiyannooryrady <raiyannooryrady@gmail.com>
2026-02-04 15:36:36 +0600

    Add boolean evaluation examples to boolean.js

---
+++ scripts/boolean.js
@@ -0,0 +1,23 @@
+let x= -14;
+console.log(Boolean(x)); //true
+
+x="false";
+console.log(Boolean(x)); //true;
+
+x=false;
+console.log(Boolean(x)); //false
+
+x=0;
+console.log(Boolean(x)); //false
+
+x="";
+console.log(Boolean(x)); //false
+
+x=undefined;
+console.log(Boolean(x)); //false
+
+x=null;
+console.log(Boolean(x)); //false
+
+x= NaN;
+console.log(Boolean(x)); //false
