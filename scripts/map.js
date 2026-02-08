25d57fdb91a3f52c2d5fd3a341a7b8ac622c4d32
raiyannooryrady <raiyannooryrady@gmail.com>
2026-02-08 12:55:48 +0600

    map

---
+++ scripts/map.js
@@ -0,0 +1,24 @@
+const fruits= new Map([
+    ["apple",500],
+    ["banana",400],
+    ["orange",200]
+]);
+fruits.set("pineapple",800);
+console.log(fruits.get("apple"));
+console.log(fruits);
+console.log(fruits.size);
+fruits.delete("banana");
+console.log(fruits);
+console.log(fruits.has("apple"));
+let text= 'Fruits Names: \n';
+fruits.forEach(myFunction);
+function myFunction(value,key){
+    text+=key+": "+value+"\n";
+}
+console.log(text);
+let entryText='';
+for (let x of fruits.entries()){
+    entryText+=x;
+}
+console.log(entryText);
+
