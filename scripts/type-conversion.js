b8c124a056ba8cde5d26be6cb01db917aa5ee26a
raiyannooryrady <raiyannooryrady@gmail.com>
2026-02-14 19:09:42 +0600

    type conversion

---
+++ scripts/type-conversion.js
@@ -0,0 +1,9 @@
+let x=10.345342;
+console.log(x.toExponential(4));
+let y= 10.4;
+console.log(y.toFixed());
+y=10.6;
+console.log(y.toFixed());
+let d=new Date();
+console.log(d.toString());
+console.log(d);
