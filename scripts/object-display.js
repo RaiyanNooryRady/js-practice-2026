ef9a150ce54d98b16db003fb39e22f551d082929
raiyannooryrady <raiyannooryrady@gmail.com>
2026-02-23 12:22:52 +0600

    object display

---
+++ scripts/object-display.js
@@ -0,0 +1,11 @@
+const person ={
+    name: 'raiyan',
+    age: 28,
+    city: 'Sylhet'
+}
+let text=`<h2>Object Display</h2><ul class='person-list'>`;
+for(let key in person){
+    text+='<li>'+key+' is: '+person[key]+'</li>';
+}
+text+='</ul>'
+document.getElementById('demo').innerHTML=text;
