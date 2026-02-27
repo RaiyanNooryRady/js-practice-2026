1b449befc1ed3af654595cbcf0b2fe4d07b80e97
raiyannooryrady <raiyannooryrady@gmail.com>
2026-02-28 04:27:08 +0600

    iterable

---
+++ scripts/iterable.js
@@ -0,0 +1,61 @@
+//symbole.iterator function exists in a iterable
+const numbers= [1,3,4,52,2];
+// for (let num of numbers){
+//     console.log(num);
+// }
+console.dir(numbers);
+console.dir(numbers.at(1));
+console.dir(numbers[Symbol.iterator]()); //returns an iterator which have next() function
+let numIterator=numbers[Symbol.iterator]();
+console.log(numIterator.next());
+console.log(numIterator.next()); 
+console.log(numIterator.next());
+console.log(numIterator.next());
+console.log(numIterator.next());
+console.log(numIterator.next());
+
+//symbole.iterator function not exists in non-iterable
+const numbers1={
+    one:1,
+    two: 3,
+    three: 4,
+    four: 42
+}
+// for (let num of numbers1){
+//     console.log(num);
+// }
+console.dir(numbers1);
+console.log(numbers1);
+console.log(Object.entries(numbers1));
+//make myNumbers object iterable
+const myNumbers={};
+myNumbers[Symbol.iterator]=()=>{
+    let n=0;
+    let done=false;
+    return {
+        next(){
+            n+=10;
+            if(n==100) {
+                done = true;
+            }
+            return {
+                value: n,
+                done: done
+            }
+        }
+    }
+}
+// for (let n of myNumbers){
+//     console.log(n);
+// }
+let itMyNum= myNumbers[Symbol.iterator]();
+console.log(itMyNum.next());
+console.log(itMyNum.next());
+console.log(itMyNum.next());
+console.log(itMyNum.next());
+console.log(itMyNum.next());
+console.log(itMyNum.next());
+console.log(itMyNum.next());
+console.log(itMyNum.next());
+console.log(itMyNum.next());
+console.log(itMyNum.next());
