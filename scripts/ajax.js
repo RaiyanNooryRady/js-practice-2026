03b62743e265614b16cd44ce6056896f39778bd2
raiyannooryrady <raiyannooryrady@gmail.com>
2026-03-25 00:03:07 +0600

    Fix XMLHttpRequest setup and enhance logging in loadData function

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -8,13 +8,15 @@ function loadData(callbackF) {
     }
 
     //prepare request - methods: GET, POST, PUT, PATCH, DELETE
-    xhr.open("GET", './scripts/data.txt');
+    xhr.open("GET", './scripts/data.txt',true); //true means async, false means sync
 
     //add a request header
     xhr.setRequestHeader('MY_TEST', 'DEVSELLER');
 
     //request send
     xhr.send();
+    
+    console.log('hello');
 
     //abort
     //xhr.abort();
@@ -22,8 +24,14 @@ function loadData(callbackF) {
 function callback1(xhr) {
     const demo = document.getElementById('demo');
     demo.innerHTML = xhr.responseText;
+    console.log('response receieved');
+    console.log(xhr.getAllResponseHeaders());
+    console.log(xhr.getResponseHeader('etag'));
 }
 function callback2(xhr) {
     const demo2 = document.getElementById('demo2');
     demo2.innerHTML = xhr.responseText;
+     console.log('response receieved');
+     console.log(xhr.getAllResponseHeaders()); //all response headers
+     console.log(xhr.getResponseHeader('date')); //single response header
 }
