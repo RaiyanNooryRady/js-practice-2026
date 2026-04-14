3c7ff9b8e30015e4c2615bb7bf06a4da18719acd
raiyannooryrady <raiyannooryrady@gmail.com>
2026-04-14 12:50:30 +0600

    Refactor array handling and enhance logging; add person object definition

--- practice1/script.js
+++ practice1/script.js
@@ -6,7 +6,15 @@ arr[6]='w';
 for(let i=0;i<arr.length;i++){
     console.log(arr[i]);
 }
+arr.forEach(function(item){
+    console.log(`array item: ${item}`);
+});
 let newStr=arr.join('');
 console.log(newStr);
 x=10; //global variable without var, let or const but works in non strict mode. In strict mode it will throw an error.
 console.log(x);
+const person=[];
+person['name']='John';
+person['age']=30;
+console.log(typeof person, typeof arr);
+console.dir(person);
