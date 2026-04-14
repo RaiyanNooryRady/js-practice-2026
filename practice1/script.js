c5d77bd8cb9a930fd8701b5e4a091f0db03de3ea
raiyannooryrady <raiyannooryrady@gmail.com>
2026-04-14 16:22:19 +0600

    Refactor script.js to comment out unused code and retain array manipulation examples

--- practice1/script.js
+++ practice1/script.js
@@ -1,26 +1,31 @@
-let str='Hello World';
-let arr=str.split('');
+// let str='Hello World';
+// let arr=str.split('');
+// console.log(arr);
+// arr[0]='h';
+// arr[6]='w';
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+// arr.forEach(function(item){
+//     console.log(`array item: ${item}`);
+// });
+// let newStr=arr.join('');
+// console.log(newStr);
+// x=10; //global variable without var, let or const but works in non strict mode. In strict mode it will throw an error.
+// console.log(x);
+// const person=[];
+// person['name']='John';
+// person['age']=30;
+// console.log(typeof person, typeof arr);
+// console.dir(person);
+// console.log(person instanceof Array);
+// const student={
+//     name:'John',
+//     age:30,
+// }
+// console.log(student instanceof Array)
+const arr=[1,2,3];
+arr.shift(); //removes first element
 console.log(arr);
-arr[0]='h';
-arr[6]='w';
-for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);
-}
-arr.forEach(function(item){
-    console.log(`array item: ${item}`);
-});
-let newStr=arr.join('');
-console.log(newStr);
-x=10; //global variable without var, let or const but works in non strict mode. In strict mode it will throw an error.
-console.log(x);
-const person=[];
-person['name']='John';
-person['age']=30;
-console.log(typeof person, typeof arr);
-console.dir(person);
-console.log(person instanceof Array);
-const student={
-    name:'John',
-    age:30,
-}
-console.log(student instanceof Array)
+arr.unshift(0); //adds element at the beginning
+console.log(arr);
