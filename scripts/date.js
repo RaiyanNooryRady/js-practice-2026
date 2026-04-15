b6fdcc2186c8a459f3e4230ab5d8ab21f3563c2a
raiyannooryrady <raiyannooryrady@gmail.com>
2026-04-15 16:26:46 +0600

    Add date.js to demonstrate Date object usage in JavaScript

---
+++ scripts/date.js
@@ -0,0 +1,5 @@
+const d= new Date(2027, 7, 24); // Year, Month (0-indexed), Day
+console.log(d.getDate()); // 24
+console.log(d.getMonth()); // 7 (August)
+console.log(d.getFullYear()); // 2027
+console.log(d);
